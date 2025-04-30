89f86c0a2ca005e896aa9ebf4ecaf55f0b587c31
tonsisouvanh <tonsisouvanh7@gmail.com>
2025-04-30 18:44:39 +0700

    init project with shadcnui installed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <div className="max-w-xl mx-auto w-full border">
+        <Button>Click me</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
